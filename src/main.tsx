eb7c6af2b922a1f4e46e6400b10fa27eb13e6063
Vadim <Vadimbureev380@gmail.com>
2025-04-14 15:07:22 +0300

    domain fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import './index.css'
 import App from './App.tsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import { UserProvider } from './context/UserContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <HashRouter>
+  <BrowserRouter>
     <UserProvider>
       <App />
     </UserProvider>
-  </HashRouter>
+  </BrowserRouter>
 )
