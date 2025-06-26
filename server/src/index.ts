98dee0ded5e95479fe13bd799a579b7d9267a09a
Vadim <Vadimbureev380@gmail.com>
2025-06-26 20:35:26 +0300

    ndm8

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://lipart.vercel.app',
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // POST /send-email
